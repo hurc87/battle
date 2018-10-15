45efe613511a943b687e76a8866264cabd95d441
hurc87 <zoe1987@btinternet.com>
2018-10-15 16:10:53 +0100

    created views folder

--- app.rb
+++ app.rb
@@ -3,16 +3,6 @@ require 'shotgun'
 
 set:session_secret, 'super secret'
 
-get '/' do
-  "Hello World"
-end
-
-get '/secret' do
-  "ThIs Is A bIgGeR tEsT"
-end
-
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
