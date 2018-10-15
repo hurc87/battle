dba64a5d07a554f337af47dadfcb8d06c7f2a5eb
hurc87 <zoe1987@btinternet.com>
2018-10-15 15:34:40 +0100

    added cat pic

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
+require 'shotgun'
+
+set:session_secret, 'super secret'
 
 get '/' do
   "Hello World"
 end
 
 get '/secret' do
-  "This is a test"
+  "ThIs Is A bIgGeR tEsT"
+end
+
+get '/cat' do
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
